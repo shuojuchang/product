2e5f387b2dd03406534741cb3f4666201d00eca9
John Doe <oulalatresbelle1210@gmail.com>
2023-09-09 21:42:47 +0800

    a complete code to record the sales record

--- product.py
+++ product.py
@@ -1,12 +1,29 @@
-products = []
+#讀取檔案
+items = []
+with open('items.csv', 'r', encoding='utf-8') as f:
+    for line in f:
+        if 'Product name,Price' in line:
+            continue  #continue/break都必須在迴圈裡意指跳出或跳過
+        name, price = line.strip().split(',')
+        items.append([name, price])
+print(items)
+
+#讓使用者輸入
 while True:
     name = input('請輸入商品名稱: ')
     if name == 'q':
         break
     price = input('請輸入商品價格: ')
-    products.append([name, price])
-print(products)
+    price = int(price)
+    items.append([name, price])
+print(items)
 
-#逐筆印出小清單
-for p in products:
-    print(p[0], '的價格是: ', p[1])
+#印出所有購買紀錄
+for p in items:
+    print(p[0], '的價格是', p[1])
+
+#寫入檔案
+with open('items.csv', 'w', encoding='utf-8') as f:
+    f.write('Product name,Price\n')
+    for p in items:
+        f.write(p[0] + ',' + str(p[1]) + '\n')

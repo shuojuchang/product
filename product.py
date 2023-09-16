f2d6c051acdccc33a7e7eb04e1dbd31ce512f9ca
John Doe <oulalatresbelle1210@gmail.com>
2023-09-16 22:43:57 +0800

    refactor update

--- product.py
+++ product.py
@@ -3,17 +3,12 @@ import os #載入operating system
 #讀取檔案
 def read_file(filename):
     items = []
-    if os.path.isfile(filename): #檢查檔案在不在
-        print('yeah I found it!')
-        with open(filename, 'r', encoding='utf-8') as f:
-            for line in f:
-                if 'Product name,Price' in line:
-                    continue  #continue/break都必須在迴圈裡意指跳出或跳過
-                name, price = line.strip().split(',')
-                items.append([name, price])
-        print(items)
-    else:
-        print('oops ! the file is missing.')
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            if 'Product name,Price' in line:
+                continue  #continue/break都必須在迴圈裡意指跳出或跳過
+            name, price = line.strip().split(',')
+            items.append([name, price])
     return items
 
 #讓使用者輸入
@@ -41,7 +36,21 @@ def write_file(filename, items):
             f.write(p[0] + ',' + str(p[1]) + '\n')
 
 
-items = read_file('items.csv')   #items.csv是這次尋找的檔名,即是def設定參數時的變數,下次找其他檔名亦不須進到程式碼一個一個修改
-items = user_input(items)        #有return時才需要 回傳 items = user_input(items)
-print_products(items)            #因此為印出項目,故不需要回傳
-write_file('items.csv', items)   #因為此為寫入檔案,故不需要回傳
+#事實上def最好只處理一件事情,這樣比較好debug
+#執行程式碼主要也會寫成一個main function
+
+def main():
+    filename = 'items.csv' 
+    if os.path.isfile(filename): #檢查檔案在不在
+        print('Yeah I found it!')
+        items = read_file(filename)   #items.csv是這次尋找的檔名,即是def設定參數時的變數,下次找其他檔名亦不須進到程式碼一個一個修改
+    else:
+        print('oops ! the file is missing.')
+
+    items = user_input(items)        #有return時才需要 回傳 items = user_input(items)
+    print_products(items)            #因此為印出項目,故不需要回傳
+    write_file(filename, items)   #因為此為寫入檔案,故不需要回傳
+
+main()
+
+#設立程式碼為function稱為refactor 重構(重新架構)
